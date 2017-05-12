de9527c8accd73e2a8bd47e11d28646eb432b278
apesconsole <apesconsole@gmail.com>
2017-05-11 20:40:44 -0500

    Commit

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ var updateDeviceInfo = function( _device ){
 				var data = {
 					status: _device.status == 1 ? true : (_device.status == 0 ) ? false : ( _device.status ) 
 				};
-				logger.log(data.type);
+				logger.log(device.type);
 				if(device.type == "S"){
 					//Sensor Data
 					logger.log(data.value);
